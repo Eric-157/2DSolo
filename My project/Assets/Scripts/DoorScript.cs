9a1c3a855a6697a9d037ebfc9d169f12aa513bcc
Eric Edwards <59770361+Eric-157@users.noreply.github.com>
2025-05-02 11:27:21 -0400

    art and kinda scene overlay

--- My project/Assets/Scripts/DoorScript.cs
+++ My project/Assets/Scripts/DoorScript.cs
@@ -4,13 +4,25 @@ using UnityEngine.SceneManagement;
 public class DoorScript : MonoBehaviour
 {
     public int sceneToLoad;
+    public int sceneToUnload;
     private bool playerInDoor = false;
+    public bool interior = false;
 
     void Update()
     {
         if (playerInDoor && Input.GetKeyDown(KeyCode.E))
         {
-            SceneManager.LoadScene(sceneToLoad);
+            if (interior == false)
+            {
+                SceneManager.LoadScene(sceneToLoad, LoadSceneMode.Additive);
+                SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex(sceneToLoad));
+            }
+            else if (interior == true)
+            {
+                SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex(sceneToLoad));
+                SceneManager.UnloadSceneAsync(SceneManager.GetSceneByBuildIndex(sceneToUnload), UnloadSceneOptions.UnloadAllEmbeddedSceneObjects);
+            }
+
         }
     }
 
